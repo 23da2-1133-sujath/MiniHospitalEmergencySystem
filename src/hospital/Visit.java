b06307459c3a4caa47475d363efd06c37de9d06b
23da2-1133-sujath <23da2-1133@sltc.ac.lk>
2026-09-02 20:07:22 +0530

    Added diagnosis and treatment details with visit display method

--- src/hospital/Visit.java
+++ src/hospital/Visit.java
@@ -7,17 +7,48 @@ public class Visit {
     int visitID;
     String visitDate;
     String doctorName;
+    String diagnosis;
+    String treatment;
+
 
 
 
     public Visit(int visitID,
                  String visitDate,
-                 String doctorName){
+                 String doctorName,
+                 String diagnosis,
+                 String treatment){
+
 
 
         this.visitID = visitID;
         this.visitDate = visitDate;
         this.doctorName = doctorName;
+        this.diagnosis = diagnosis;
+        this.treatment = treatment;
+
+
+
+    }
+
+
+
+
+
+
+    public void displayVisit(){
+
+
+        System.out.println("--------------------------");
+
+        System.out.println("Visit ID    : " + visitID);
+        System.out.println("Date        : " + visitDate);
+        System.out.println("Doctor      : " + doctorName);
+        System.out.println("Diagnosis   : " + diagnosis);
+        System.out.println("Treatment   : " + treatment);
+
+
+        System.out.println("--------------------------");
 
 
     }
