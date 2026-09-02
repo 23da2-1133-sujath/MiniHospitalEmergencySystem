aecaf0ae6151a371e4e06b8eda7d66c56e39526e
23da2-1133-sujath <23da2-1133@sltc.ac.lk>
2026-09-02 19:53:05 +0530

    Added patient contact and medical condition details with display method

--- src/hospital/Patient.java
+++ src/hospital/Patient.java
@@ -5,15 +5,43 @@ public class Patient {
     int patientID;
     String name;
     int age;
+    String contactNumber;
+    String medicalCondition;
 
 
-    public Patient(int patientID, String name, int age){
+
+    public Patient(int patientID, 
+                   String name, 
+                   int age,
+                   String contactNumber,
+                   String medicalCondition){
+
 
         this.patientID = patientID;
         this.name = name;
         this.age = age;
+        this.contactNumber = contactNumber;
+        this.medicalCondition = medicalCondition;
+
 
     }
 
+
+
+    public void displayPatient(){
+
+
+        System.out.println("---------------------------");
+
+        System.out.println("Patient ID : " + patientID);
+        System.out.println("Name       : " + name);
+        System.out.println("Age        : " + age);
+        System.out.println("Contact    : " + contactNumber);
+        System.out.println("Condition  : " + medicalCondition);
+
+        System.out.println("---------------------------");
+
+
+    }
 
 }
