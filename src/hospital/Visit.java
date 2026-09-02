b89883ae8035e82b057eed1e2070d3a8419ee55b
23da2-1133-sujath <23da2-1133@sltc.ac.lk>
2026-09-02 20:05:48 +0530

    Created Visit class with basic visit information

---
+++ src/hospital/Visit.java
@@ -0,0 +1,26 @@
+package hospital;
+
+
+public class Visit {
+
+
+    int visitID;
+    String visitDate;
+    String doctorName;
+
+
+
+    public Visit(int visitID,
+                 String visitDate,
+                 String doctorName){
+
+
+        this.visitID = visitID;
+        this.visitDate = visitDate;
+        this.doctorName = doctorName;
+
+
+    }
+
+
+}
