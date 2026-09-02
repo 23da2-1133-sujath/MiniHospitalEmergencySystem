07ca11e1f24f83f16ccdbdad580ef458a986194f
23da2-1133-sujath <23da2-1133@sltc.ac.lk>
2026-09-02 19:51:18 +0530

    Created Patient class with basic patient attributes

---
+++ src/hospital/Patient.java
@@ -0,0 +1,19 @@
+package hospital;
+
+public class Patient {
+
+    int patientID;
+    String name;
+    int age;
+
+
+    public Patient(int patientID, String name, int age){
+
+        this.patientID = patientID;
+        this.name = name;
+        this.age = age;
+
+    }
+
+
+}
